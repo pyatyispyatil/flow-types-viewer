e0c23e883f4b07f654adb0a1b9a758a7bd4f4a1e
Nikolay Ryabov <pyatyispyatil@gmail.com>
2018-03-07 22:37:20 +0300

    add some types handling on ui

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const exec = require('child_process').execSync;
 const fs = require('fs');
+const rm = require('rimraf');
 
 const parser = require('./parser');
 const {getDeclarations} = require('./utils');
 
-const [,,index] = process.argv;
+const [, , index] = process.argv;
 
 if (index) {
   const ast = parser.makeAST(index);
   const declarations = getDeclarations([index], ast);
   const html = fs.readFileSync('./template.html').toString().replace(/{{data}}/igm, JSON.stringify(declarations));
 
-  fs.mkdirSync('build');
-  exec('npm run build');
+  rm.sync('build');
 
+  fs.mkdirSync('build');
   fs.writeFileSync('./build/index.html', html);
 }
--- viewer.js
+++ viewer.js
@@ -1,14 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class TreeItem extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    )
-  }
-}
 
 class Tree extends Component {
   state = {
@@ -17,28 +9,64 @@ class Tree extends Component {
 
   handleClick = () => this.setState({collapsed: !this.state.collapsed});
 
+  renderNode() {
+    const {node} = this.props;
+
+    switch (node.type) {
+      case 'type':
+        return <Tree node={DATA.declarations[node.declarationId]}/>;
+      case 'intersection':
+        return (
+          <div>
+            {node.value.map((item) => (
+              <Tree node={item}/>
+            ))}
+          </div>
+        );
+      case 'object':
+        return (
+          <div>
+            {'{'}
+            {node.value.map(({value, name}) => (
+              <div>
+              {`${name}: `}
+              <Tree node={value}/>
+              </div>
+              ))
+            }
+            {'}'}
+          </div>
+        );
+      case 'primitive':
+        return node.value;
+      default:
+        return 'unhandled';
+    }
+  }
+
   render() {
     const {collapsed} = this.state;
-    const {node, declarations} = this.props;
+    const {node} = this.props;
 
     return (
-      <div className={'tree-view'}>
-        <div className={'tree-view_item'}>
-          <div onClick={this.handleClick}/>
-          {node.name}
-        </div>
+      node.name ? (
         <div>
-          {collapsed ? null : <Tree/>}
+          <div className={'tree-view_item'} onClick={this.handleClick}>
+            {node.name}
+          </div>
+          <div>
+            {collapsed ? null : this.renderNode()}
+          </div>
         </div>
-      </div>
-    );
+      ) : (this.renderNode())
+    )
   }
 }
 
 ReactDOM.render((
   <div>
     {
-      DATA.types.map((type) => <Tree node={type} declarations={DATA.declarations}/>)
+      DATA.types.map((type) => <Tree node={type}/>)
     }
   </div>
 ), document.getElementById('root'));
